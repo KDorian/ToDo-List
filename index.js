86c1ae3b41303c2b34348364b73f7721a221011d
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-09-17 16:33:40 +0200

    Add checklist

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-    // YOUR CODE HERE!
+    $(".add-items").submit(function(event) {
+      event.preventDefault();
+
+      var item = $("#todo-list-item").val();
+
+      if (item) {
+        $("#list-items").append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>x</a></li>");
+        $("#todo-list-item").val("");
+      }
+    });
+
+    $(document).on("change", ".checkbox", function() {
+      $(this).parent().toggleClass("completed");
+    });
 });
